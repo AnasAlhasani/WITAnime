0871f6242da2ed1e27b67af0385696d34c23a9e5
Anas Alhasani <anasalhassni@gmail.com>
2024-04-18 17:12:23 +0300

    Make logger public

--- Sources/WITAnime/Utilities/Logger.swift
+++ Sources/WITAnime/Utilities/Logger.swift
@@ -1,21 +1,21 @@
 import Foundation
 import os
 
-let logger = Logger(
+public let logger = Logger(
     subsystem: "com.witanime",
     category: "WITAnime"
 )
 
-struct Logger {
+public struct Logger {
     private let logger: os.Logger
     
-    init(subsystem: String, category: String) {
+    public init(subsystem: String, category: String) {
         logger = os.Logger(subsystem: subsystem, category: category)
     }
 }
 
 extension Logger {
-    func log(
+    public func log(
         level: OSLogType = .default,
         _ message: @autoclosure () -> String,
         file: String = URL(fileURLWithPath: #fileID).deletingPathExtension().lastPathComponent,
